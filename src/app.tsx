b74919fcf33b0145efd2f6b523312d7c162a3534
dalramjwi <dalramjwi@gmail.com>
2024-07-10 12:43:32 +0900

    css styles 지정

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+interface loginStyle {
+  width: string;
+  height: string;
+  backgroundColor: string;
+}
 const App = () => {
-  return <h1>asd</h1>;
+  let styles: React.CSSProperties = {
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "#f5f5f5",
+  };
+  return (
+    <div>
+      <div id="login" style={styles}>
+        <div id="login-input"></div>
+        <div id="login-submit"></div>
+      </div>
+    </div>
+  );
 };
 export default App;
